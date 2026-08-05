2a59e4e1d698fe189e2b1e72fd8cdaf5b520258f
Aditya Agarwal <2k24.cs1b.2412314@gmail.com>
2026-08-06 01:04:22 +0530

    Time: 7 ms (63.40%) | Memory: 43.9 MB (95.23%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,16 +1,85 @@
 class Solution {
     public String reverseWords(String s) {
-        String trim=s.trim();
-        String arr[]=trim.split("\\s+");
-        //[hello,world]
-        int i=0;
-        int j=arr.length-1;
-        while(i<j){
-            String temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
-            i++;j--;
+
+        // Remove leading spaces
+        int l = 0;
+        while (l < s.length() && s.charAt(l) == ' ') {
+            l++;
         }
-        return String.join(" ",arr);
+
+        // Remove trailing spaces
+        int r = s.length() - 1;
+        while (r >= l && s.charAt(r) == ' ') {
+            r--;
+        }
+
+        // Remove extra spaces between words
+        StringBuilder sb = new StringBuilder();
+
+        while (l <= r) {
+            if (s.charAt(l) != ' ') {
+                sb.append(s.charAt(l));
+            } else {
+                if (sb.length() > 0 && sb.charAt(sb.length() - 1) != ' ') {
+                    sb.append(' ');
+                }
+            }
+            l++;
+        }
+
+        // Reverse the whole StringBuilder
+        int left = 0;
+        int right = sb.length() - 1;
+
+        while (left < right) {
+            char temp = sb.charAt(left);
+            sb.setCharAt(left, sb.charAt(right));
+            sb.setCharAt(right, temp);
+            left++;
+            right--;
+        }
+
+        // Reverse each word
+        int start = 0;
+
+        while (start < sb.length()) {
+
+            int end = start;
+
+            while (end < sb.length() && sb.charAt(end) != ' ') {
+                end++;
+            }
+
+            left = start;
+            right = end - 1;
+
+            while (left < right) {
+                char temp = sb.charAt(left);
+                sb.setCharAt(left, sb.charAt(right));
+                sb.setCharAt(right, temp);
+                left++;
+                right--;
+            }
+
+            start = end + 1;
+        }
+
+        return sb.toString();
     }
 }
+// class Solution {
+//     public String reverseWords(String s) {
+//         String trim=s.trim();
+//         String arr[]=trim.split("\\s+");
+//         //[hello,world]
+//         int i=0;
+//         int j=arr.length-1;
+//         while(i<j){
+//             String temp=arr[i];
+//             arr[i]=arr[j];
+//             arr[j]=temp;
+//             i++;j--;
+//         }
+//         return String.join(" ",arr);
+//     }
+// }
