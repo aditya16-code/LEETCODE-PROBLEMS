c86d5bd10809d1815edddc3c1280bb2d7ea3e359
Aditya Agarwal <2k24.cs1b.2412314@gmail.com>
2026-08-09 16:11:32 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (73.42%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,15 +11,45 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        Set<ListNode>set=new HashSet<>();
-        ListNode curr=head;
-        while(curr!=null){
-        if(set.contains(curr)){
-             return true;}
-        set.add(curr);
-        curr=curr.next;
-        }
+        if(head==null)
         return false;
+        ListNode slow=head;
+        ListNode fast=head.next;
         
+        while(slow!=fast){
+            if(fast==null||fast.next==null)
+            return false;
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        if(fast==slow)
+        return true;
+        return false;
     }
+
 }
+/**
+//  * Definition for singly-linked list.
+//  * class ListNode {
+//  *     int val;
+//  *     ListNode next;
+//  *     ListNode(int x) {
+//  *         val = x;
+//  *         next = null;
+//  *     }
+//  * }
+//  */
+// public class Solution {
+//     public boolean hasCycle(ListNode head) {
+//         Set<ListNode>set=new HashSet<>();
+//         ListNode curr=head;
+//         while(curr!=null){
+//         if(set.contains(curr)){
+//              return true;}
+//         set.add(curr);
+//         curr=curr.next;
+//         }
+//         return false;
+        
+//     }
+// }
