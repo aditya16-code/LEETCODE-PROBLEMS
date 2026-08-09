acb47c3401d4cf98ef381161eea7f74d6bad2f1d
Aditya Agarwal <2k24.cs1b.2412314@gmail.com>
2026-08-09 23:59:25 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (13.60%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -11,14 +11,36 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        Set<ListNode>set=new HashSet<>();
-        ListNode curr=head;
-        while(curr!=null){
-            if(set.contains(curr)){
-            return curr;}
-            set.add(curr);
-            curr=curr.next;
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null&&fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow){
+                break;
+            }
         }
+        if(fast==null||fast.next==null)
         return null;
+
+        ListNode curr=head;
+        while(curr!=slow){
+            curr=curr.next;
+            slow=slow.next;
+        }
+        return curr;
     }
 }
+// public class Solution {
+//     public ListNode detectCycle(ListNode head) {
+//         Set<ListNode>set=new HashSet<>();
+//         ListNode curr=head;
+//         while(curr!=null){
+//             if(set.contains(curr)){
+//             return curr;}
+//             set.add(curr);
+//             curr=curr.next;
+//         }
+//         return null;
+//     }
+// }
