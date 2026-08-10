026fae3ba0876f6a2828050f41cc26a5cd9471f2
Aditya Agarwal <2k24.cs1b.2412314@gmail.com>
2026-08-10 19:55:40 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (50.63%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -11,23 +11,32 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        ListNode fast=head;
-        ListNode slow=head;
-        while(fast!=null&&fast.next!=null){
-            fast=fast.next.next;
-            slow=slow.next;
-            if(fast==slow){
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (true) {
+
+            if (fast == null || fast.next == null) {
+                return null;
+            }
+
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
                 break;
             }
         }
-        if(fast==null||fast.next==null)
-        return null;
 
-        ListNode curr=head;
-        while(curr!=slow){
-            curr=curr.next;
-            slow=slow.next;
+        // Find cycle starting point
+        ListNode curr = head;
+
+        while (curr != slow) {
+            curr = curr.next;
+            slow = slow.next;
         }
+
         return curr;
     }
 }
