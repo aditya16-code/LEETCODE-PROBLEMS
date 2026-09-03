02e7ba1c4e5382267715c8c53f9c6f7732454e83
Aditya Agarwal <2k24.cs1b.2412314@gmail.com>
2026-09-03 23:08:01 +0530

    Time: 68 ms (6.82%) | Memory: 104.8 MB (5.04%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,64 +1,56 @@
-
-
-// ------------------------------------------TLEEE----------------------------------------------
-// class Solution {
-//     public long maximumSubarraySum(int[] nums, int k) {
-//         // Set<Integer>set=new HashSet<>();
-//         long max=0;
-//         // boolean dup=false;
-//         for(int i=0;i<=nums.length-k;i++){
-//             long sum=0;
-//             boolean dup=false;
-//         Set<Integer>set=new HashSet<>();
-//             for(int j=i;j<=i+k-1;j++){
-//                if(set.contains(nums[j])){
-//                 dup=true;
-//                 break;
-//                }
-//                set.add(nums[j]);
-//                sum=sum+nums[j];
-//             //    max=Math.max(sum,max);
-//             }
-//             if(!dup){
-//                 max=Math.max(sum,max);
-//             }
-
-//         }
-//         return max;
-//     }
-// }
-//--------------------------------------------------------------------------------------------
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        int n = nums.length;
+        long max=0,sum=0;int dups=0;
 
-        HashSet<Integer> set = new HashSet<>();
+        Map<Integer,Integer>map=new HashMap<>();
 
-        long sum = 0;
-        long maxSum = 0;
+        for(int i=0;i<k;i++){
 
-        int left = 0;
+            if(map.containsKey(nums[i])){
 
-        for (int right = 0; right < n; right++) {
-
-            while (set.contains(nums[right])) {
-                set.remove(nums[left]);
-                sum -= nums[left];
-                left++;
+                map.put(nums[i],map.get(nums[i])+1);
             }
+            else{ map.put(nums[i],0);
+}
+           
+            sum=sum+nums[i];
 
-            set.add(nums[right]);
-            sum += nums[right];
-
-            if (right - left + 1 == k) {
-                maxSum = Math.max(maxSum, sum);
-
-                set.remove(nums[left]);
-                sum -= nums[left];
-                left++;
+            if(map.get(nums[i])>=1){
+                dups++;
             }
+            
+            
         }
+        if(dups==0){
+                max=Math.max(sum,max);
+            }
+        for(int i=k;i<nums.length;i++){
+            int numtoadd=nums[i];
+            int numtoremove=nums[i-k];
+           if(map.containsKey(numtoadd)){
+            map.put(numtoadd,map.get(numtoadd)+1);
+           }
+           else{map.put(numtoadd,0);}
+           
+        if(map.get(numtoadd)>=1){
+            dups=dups+1;
+        }
+        sum=sum+nums[i];
 
-        return maxSum;
+      if(map.get(numtoremove)>=1){
+        dups=dups-1;
+      }
+      map.put(numtoremove,map.get(numtoremove)-1);
+       sum=sum-numtoremove;
+       if(dups==0){
+        max=Math.max(sum,max);
     }
+        }
+    
+        
+      
+ return max;
+
+    }
+   
 }
