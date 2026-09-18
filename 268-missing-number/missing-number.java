f741fa36dac5510d8dafa8b940df8e089283979f
Aditya Agarwal <2k24.cs1b.2412314@gmail.com>
2026-09-18 19:56:51 +0530

    Time: 5 ms (21.47%) | Memory: 47.5 MB (19.77%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -4,12 +4,12 @@ class Solution {
      for(int i=0;i<nums.length;i++){
         set.add(nums[i]);
      }
-     int j=0;
+
      for(int i=0;i<=nums.length;i++){
         if(!set.contains(i)){
-            j=i;
+           return i;
         }
      }
-     return j;
+     return -1;
     }
 }
