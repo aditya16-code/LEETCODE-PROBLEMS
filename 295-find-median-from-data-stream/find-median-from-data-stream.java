5ffc8f4a3c1fafa8c12c6e374a0ce7bcf264ba53
Aditya Agarwal <2k24.cs1b.2412314@gmail.com>
2026-09-22 16:17:37 +0530

    Time: 127 ms (82.76%) | Memory: 112.3 MB (6.87%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -1,37 +1,37 @@
 class MedianFinder {
-
-    PriorityQueue<Integer> left;
-    PriorityQueue<Integer> right;
-
+    PriorityQueue<Integer>left;
+    PriorityQueue<Integer>right;
     public MedianFinder() {
-        left = new PriorityQueue<>(Collections.reverseOrder());
-        right = new PriorityQueue<>();
+    left=new PriorityQueue<>(Collections.reverseOrder());
+    right=new PriorityQueue<>();
     }
-
+    
     public void addNum(int num) {
-
-        if (left.isEmpty() || num <= left.peek()) {
+        if(left.size()==0||num<=left.peek()){
             left.add(num);
-        } else {
+        }else{
             right.add(num);
         }
-
-        // Balance heaps
-        if (left.size() > right.size() + 1) {
-            right.add(left.poll());
+        if(left.size()>right.size()+1){
+            right.offer(left.poll());
         }
+        if(right.size()>left.size()){
+            left.offer(right.poll());
 
-        if (right.size() > left.size()) {
-            left.add(right.poll());
         }
     }
-
+    
     public double findMedian() {
-
-        if (left.size() > right.size()) {
+        if(left.size()>right.size()){
             return left.peek();
         }
-
-        return (left.peek() + right.peek()) / 2.0;
+        return (left.peek()+right.peek())/2.0;
     }
 }
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
